32ee9bf565cfa891fb80e0427e8f8ba247bdf90f
Adi Rabinovich <adir1@yahoo.com>
2024-10-01 01:04:30 -0400

    AI generated image options for site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+// Logo Dall-E Prompt: Please create an SVG image of seed of life merkabah and cloud, against shining light background
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Merka.Cloud Education',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/adir1/merka.cloud',
 			},
 			sidebar: [
 				{
